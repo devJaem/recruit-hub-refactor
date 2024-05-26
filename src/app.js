40e589661f814c7f69bcf86208839b1e8852c517
jaemin <wjdwoals9292@gmail.com>
2024-05-26 15:35:30 +0900

    [HOTFIX] 오탈자 수정

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import { ENV } from './constants/env.constant.js';
-import { errorHandler} from './middlewares/error-handling.middleware.js';
-import LogMiddleware from './middlewares/log.middleware.js';
+import { errorHandler } from './middlewares/error-handling.middleware.js';
+import logMiddleware from './middlewares/log.middleware.js';
 import index from './routers/index.js';
 
 const app = express();
 const PORT = ENV.PORT;
 
-app.use(LogMiddleware);
+app.use(logMiddleware);
 app.use(express.json());
-app.use(cookieParser());
-app.use(errorHandler);
 app.use('/api/v1',[index]);
+app.use(errorHandler);
 
 app.get('/', async (req, res) => {
   res.status(200).json({ message: '서버 정상 동작중'});
