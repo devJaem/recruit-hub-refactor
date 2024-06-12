665158a6456777986df7011be54d410cc4cc6583
jaemin <wjdwoals9292@gmail.com>
2024-06-12 22:53:47 +0900

    [feature] test코드 작성 준비

---
+++ __tests__/dummies/users.dummy.js
@@ -0,0 +1,38 @@
+export const dummyUsers = [
+  {
+    userId: 1,
+    email: 'spartan@spartacodingclub.kr',
+    password: '$2b$10$ZU8QqLH0phwjorogYV67jOl6x5l/pm5Oc1QcNzOgz6ADzy7ntQ2FG',
+    userInfo: {
+      userInfoId: 1,
+      name: '스파르탄',
+      role: 'APPLICANT',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    userId: 2,
+    email: 'applicant@spartacodingclub.kr',
+    password: '$2b$10$ZU8QqLH0phwjorogYV67jOl6x5l/pm5Oc1QcNzOgz6ADzy7ntQ2FG',
+    userInfo: {
+      userInfoId: 2,
+      name: '지원자',
+      role: 'APPLICANT',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+  {
+    userId: 3,
+    email: 'recruiter@spartacodingclub.kr',
+    password: '$2b$10$ZU8QqLH0phwjorogYV67jOl6x5l/pm5Oc1QcNzOgz6ADzy7ntQ2FG',
+    userInfo: {
+      userInfoId: 3,
+      name: '채용 담당자',
+      role: 'RECRUITER',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  },
+];
